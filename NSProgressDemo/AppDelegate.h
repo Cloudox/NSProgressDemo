1e929325fc8909ae4b8c2078bff0d5c84bdaa010
Cloudox <cloudox@foxmail.com>
2016-12-01 15:43:04 +0800

    Initial Commit

---
+++ NSProgressDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSProgressDemo
+//
+//  Created by csdc-iMac on 2016/12/1.
+//  Copyright © 2016年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSProgressDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSProgressDemo
+//
+//  Created by csdc-iMac on 2016/12/1.
+//  Copyright © 2016年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
